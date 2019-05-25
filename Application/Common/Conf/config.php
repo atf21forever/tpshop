eb340fbc1f2f8b09aa5ed81a42549f77e516b765
hzj <atf21forever@126.com>
2019-05-25 15:09:38 +0800

    数据库连接

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,10 +6,10 @@ return array(
 
     //数据库配置信息
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'test', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '8258651', // 密码
+    'DB_HOST' => '10.50.5.133', // 服务器地址
+    'DB_NAME' => 'tpshop', // 数据库名
+    'DB_USER' => 'tpshop', // 用户名
+    'DB_PWD' => '123456', // 密码
     'DB_PORT' => 3306, // 端口
   
 
