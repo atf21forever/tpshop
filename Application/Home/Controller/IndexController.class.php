c662f87cf1e6cff53867e5e87022ac95f87fb0b0
wch <1836367433@qq.com>
2019-05-25 12:04:51 +0800

    备注信息

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,144 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+public function index(){
+  
+  $data['name']=I('post.name');
+  $data['pass']=I('post.pass');
+  $name=$data['name'];
+  $pass=$data['pass']; 
+
+ $add=M('tp_regis')->where("name='$name'")->find();
+ 
+ if($add){
+      
+   
+    if($pass==$add['pass']){
+  
+  	    $this->display();
+  
+     }else{
+     
+        $this->display('login');
+     }
+     
+ 
+ }else{
+ 
+  $this->display('login');
+ 
+ }
+
+    }
+
+//登录页面
+public function login(){
+
+  $this->display();
+
+}
+//注册页面跳转
+public function register(){
+
+
+	$this->display();
+}
+
+//注册页面
+public function regis(){
+
+  $data['name']=I('post.name');
+  $name=$data['name'];
+  $add=M('tp_regis')->where('name','=','$name')->find();
+
+if($add){
+
+  $data['name']=I('post.name');
+  $data['pass']=I('post.pass');
+  M('tp_regis')->add($data);	
+  $this->display('login');
+ 
+
+}else{
+$this->display('register');
+	
+}
+
+   
+} 
+
+//我的页面
+public function member(){
+
+	$this->display();
+	 
+}
+
+public function order(){
+
+ $all=M('tp_order')->where('id','>','0')->select();
+ $add=M('tp_takeout')->where('id','>','0')->select();
+// var_dump($all);
+ 
+ $this->assign('all',$all);
+ $this->assign('add',$add);
+
+
+  $this->display();
+
+}
+
+public function delete(){
+ 
+  $id=I('get.id');
+  
+ M('tp_order')->where('id','=',$id)->delete();
+ 
+ $this->display('order'); 
+
+
+}
+
+public function delete1(){
+
+  $id=I('get.id');
+  
+ M('tp_order')->where('id','=',$id)->delete();
+ 
+ $this->display('order'); 
+
+}
+
+public function comments(){
+ 
+
+$this->display();
+}
+
+public function comments1(){
+
+$name=I('get.name');
+
+var_dump($name);
+}
+
+public function tof(){
+$all=M('tp_tof')->where('id','>','0')->select();
+//var_dump($all);
+$this->assign('all',$all);
+
+$this->display();
+
+}
+
+
+
+
+
+
+
+}
+
+
+
---
+++ Application/Runtime/Cache/Home/0034c409d5ac2c07ac3f17b1b2ad5d84.php
@@ -0,0 +1,112 @@
+<?php if (!defined('THINK_PATH')) exit();?>﻿<!DOCTYPE html>
+<html>
+<head>
+   	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="懒虫商城" />
+    <meta name="keywords" content="懒虫商城" />
+    <meta name="author" content="懒虫商城" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="/Public/Home/css/swiper.min.css" />
+    <link rel="stylesheet" type="text/css" href="/Public/Home/css/style.css" />
+
+
+    <script type="text/javascript" src="/Public/Home/js/jquery.js"></script>
+    <script type="text/javascript" src="/Public/Home/js/custom.js"></script>
+
+    <title>懒虫商城-个人中心</title>
+   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0, user-scalable=no" />
+    <link rel="shortcut icon" href="Images/ico/favicon.ico" type="image/x-icon">
+  
+</head>
+<body>
+
+    <div class="ng-scope">
+        <div class="header ng-scope">
+
+            <img src="/Public/Home/img/tou.png" class="img">
+            <p class="mobile ng-binding">15625632565</p>
+            <p class="nickname ng-binding">阿拉伯联合酋长国-村长</p>
+        </div>
+        <div style="clear:both;background:#E0E0E0;height:5px;" class="ng-scope"></div>
+        <ul class="headerlist ng-scope">
+            <li class="ng-binding">
+                消费积分余额 ￥100.00
+            </li>
+            <li class="ng-binding">
+                累计消费 ￥0.00
+            </li>
+        </ul>
+        <div style="clear:both;background:#E0E0E0;height:5px;" class="ng-scope"></div>
+        <ul class="userlist ng-scope">
+            <li>
+                <a href="#">
+                    <p class="ico ico-reward"></p>
+                    <p>每日签到</p>
+                </a>
+            </li>
+            <li>
+                <a href="order.html">
+                    <p class="ico ico-order"></p>
+                    <p>我的订单</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <p class="ico ico-recommend"></p>
+                    <p>推荐有礼</p>
+                </a>
+            </li>
+            <li>
+                <a href="memberPoints.html">
+                    <p class="ico ico-coupon"></p>
+                    <p>积分明细</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <p class="ico ico-about"></p>
+                    <p>关于我们</p>
+                </a>
+            </li>
+            <li>
+                <a href="comments.html">
+                    <p class="ico ico-customer"></p>
+                    <p>客服/反馈</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!--底部导航-->
+    <nav class="ng-scope">
+        <ul class="nav">
+            <li>
+                <a href="shop.html">
+                    <img src="/Public/Home/img/foot01.png" alt="">
+                    <p>超市</p>
+                </a>
+            </li>
+            <li>
+                <a href="cart.html">
+                    <img src="/Public/Home/img/foot02.png" alt="">
+                    <p>购物车</p>
+                </a>
+                <i id="cartnum" class="ng-binding cartnum">0</i>
+            </li>
+            <li>
+                <a href="member.html">
+                    <img src="/Public/Home/img/foot03.png" alt="">
+                    <p>我的</p>
+                </a>
+            </li>
+            <li>
+                <a href="order">
+                    <img src="/Public/Home/img/foot04.png" alt="">
+                    <p>订单</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!--/底部导航-->
+</body>
+</html>
