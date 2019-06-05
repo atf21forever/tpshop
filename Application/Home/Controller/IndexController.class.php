1b8d6f24da1a4fc3ad5754ea065c3b47d8c22a1c
wch <1836367433@qq.com>
2019-06-05 09:37:13 +0800

    wu

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,18 +2,17 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-public function index(){
+  public function index(){
   
   $data['name']=I('post.name');
   $data['pass']=I('post.pass');
   $name=$data['name'];
   $pass=$data['pass']; 
 
- $add=M('tp_regis')->where("name='$name'")->find();
+  $add=M('tp_regis')->where("name='$name'")->find();
  
  if($add){
-      
-   
+  
     if($pass==$add['pass']){
   
   	    $this->display();
@@ -22,119 +21,150 @@ public function index(){
      
         $this->display('login');
      }
-     
- 
+
  }else{
  
-  $this->display('login');
+    $this->display('login');
  
  }
 
-    }
+}
 
 //登录页面
-public function login(){
+  public function login(){
 
-  $this->display();
+    $this->display();
 
-}
+ }
 //注册页面跳转
-public function register(){
-
-
-	$this->display();
-}
+  public function register(){
+  
+  	$this->display();
+  }
 
 //注册页面
-public function regis(){
+  public function regis(){
 
-  $data['name']=I('post.name');
-  $name=$data['name'];
-  $add=M('tp_regis')->where('name','=','$name')->find();
+    $data['name']=I('post.name');
+    $name=$data['name'];
+    $add=M('tp_regis')->where('name','=','$name')->find();
 
-if($add){
+  if($add){
 
-  $data['name']=I('post.name');
-  $data['pass']=I('post.pass');
-  M('tp_regis')->add($data);	
-  $this->display('login');
- 
+    $data['name']=I('post.name');
+    $data['pass']=I('post.pass');
+    M('tp_regis')->add($data);	
+    $this->display('login');
 
-}else{
-$this->display('register');
-	
-}
+    }else{
+    $this->display('register');
+    	
+    }
 
-   
-} 
+     
+  } 
 
 //我的页面
-public function member(){
+  public function member(){
 
-	$this->display();
-	 
-}
+  	$this->display();
+  	 
+  }
 
-public function order(){
+  public function order(){
 
- $all=M('tp_order')->where('id','>','0')->select();
- $add=M('tp_takeout')->where('id','>','0')->select();
-// var_dump($all);
- 
- $this->assign('all',$all);
- $this->assign('add',$add);
+     $all=M('tp_order')->where('id','>','0')->select();
+     $add=M('tp_takeout')->where('id','>','0')->select();
+    // var_dump($all);
+     $this->assign('all',$all);
+     $this->assign('add',$add);
+     $this->display();
+
+  }
+
+  public function delete(){
+   
+     $id=I('get.id');
+      
+     M('tp_order')->where('id','=',$id)->delete();
+     
+     $this->display('order'); 
 
 
+  }
+
+  public function delete1(){
+
+     $id=I('get.id');
+      
+     M('tp_order')->where('id','=',$id)->delete();
+     
+     $this->display('order'); 
+
+  }
+//用户咨询
+  public function comments(){
+
+     $this->display();
+  }
+
+
+
+//用户咨询
+  public function comments1(){
+
+    // $name=I('get.name');
+    $data['name']=I('get.name');
+    $data['phone']=I('get.phone');
+    $data['location']=I('get.location');
+    $data['text']=I('get.text');
+    M('tp_comment')->add($data);
+    
+    $this->display('comments');
+  }
+
+
+//外卖页面
+  public function tof(){
+    $all=M('tp_tof')->where('id','>','0')->select();
+    //var_dump($all);
+    $this->assign('all',$all);
+
+    $this->display();
+
+  }
+//结算页面
+  public function cart(){
+    // $id=I('get.id');
+    // // $id=I('get.count1');
+   
+    // $this->display();
+
+  $all=M('tp_address')->where('id','>',0)->select();
+
+  // var_dump($all);
+  $this->assign('all',$all);
   $this->display();
 
-}
+  }
 
-public function delete(){
- 
-  $id=I('get.id');
-  
- M('tp_order')->where('id','=',$id)->delete();
- 
- $this->display('order'); 
+  public function edit_address($id){
+   $this->assign('id',$id);
+   $this->display();
+  }
 
-
-}
-
-public function delete1(){
-
-  $id=I('get.id');
-  
- M('tp_order')->where('id','=',$id)->delete();
- 
- $this->display('order'); 
-
-}
-
-public function comments(){
- 
-
-$this->display();
-}
-
-public function comments1(){
-
-$name=I('get.name');
-
-var_dump($name);
-}
-
-public function tof(){
-$all=M('tp_tof')->where('id','>','0')->select();
-//var_dump($all);
-$this->assign('all',$all);
-
-$this->display();
-
-}
-
-
-
-
+//修改地址
+  public function address($id){
+    $name = I('get.name');
+    $phone = I('get.phone');
+    $selected1 = I('get.selected1');
+    $selected= I('get.selected');
+    $fang = I('get.fang');
+    M('tp_address')->where('id','=','id')->data(array('name'=>$name,'phone'=>$phone,
+    'selected1'=>I('get.selected1'),'fang'=>$fang))->save();
+    
+    $this->display('edit_address');
+  }
 
 
 
--- Application/Home/Controller/loginController.class.php
+++ Application/Home/Controller/loginController.class.php
@@ -2,15 +2,15 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+
     public function login(){
-         
         $data=M('tp_regis')->select();       
-        
         $this->ajaxReturn($data);
-
-
-        $this->display();
-    
-
+		$this->display();
     }
+
+
+
+
+
 }
